25ff551fa86ce900a9bb5e4230c0707772c15ece
Rodrigo Igawa <rodrigoigawa@Rodrigos-MacBook-Pro-2.local>
2019-11-06 21:40:31 -0300

    Adiciona main class e MF

---
+++ src/main/java/com/moskit/contatosApp/ContatosAppApplication.java
@@ -0,0 +1,13 @@
+package com.moskit.contatosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosAppApplication.class, args);
+	}
+
+}
